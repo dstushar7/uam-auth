73fc4c0edbbc93d3ad8460b0f7c9a9da0adfec6a
dstushar7 <tusharsarker97@gmail.com>
2025-07-08 19:01:59 +0600

    Implementing Security

--- src/main/java/org/dstushar7/uamauth/security/CustomUserDetailsService.java
+++ src/main/java/org/dstushar7/uamauth/security/CustomUserDetailsService.java
@@ -1,15 +1,21 @@
 package org.dstushar7.uamauth.security;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.dstushar7.uamauth.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+
 }
--- src/main/java/org/dstushar7/uamauth/security/SecurityConfig.java
+++ src/main/java/org/dstushar7/uamauth/security/SecurityConfig.java
@@ -1,15 +1,14 @@
 package org.dstushar7.uamauth.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
 
 }
