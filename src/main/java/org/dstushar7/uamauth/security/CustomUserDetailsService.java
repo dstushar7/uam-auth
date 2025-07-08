46611aec2de6decbd4a1a93ae17847e683583c9b
dstushar7 <tusharsarker97@gmail.com>
2025-07-09 02:06:31 +0600

    Building CustomUserDetailsService to fetch User data from  DB and give it to Spring

--- src/main/java/org/dstushar7/uamauth/security/CustomUserDetailsService.java
+++ src/main/java/org/dstushar7/uamauth/security/CustomUserDetailsService.java
@@ -1,11 +1,17 @@
 package org.dstushar7.uamauth.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.dstushar7.uamauth.entity.User;
 import org.dstushar7.uamauth.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +20,19 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional(readOnly = true)         // Ensures consistent read if user is being modified elsewhere
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: "+ username));
 
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRole().getAuthority())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(!user.isEnabled())
+                .build();
+    }
 }
